9c467ba5c2885f06a4396f74ec4d6515723cf41c
Tony Reina <37851530+tonyreina@users.noreply.github.com>
2020-12-29 20:54:52 -0800

    Update keras_mnist_hvd.py

--- keras_mnist_hvd.py
+++ keras_mnist_hvd.py
@@ -1,3 +1,17 @@
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from tensorflow import keras as K
 import tensorflow as tf
 import horovod.tensorflow.keras as hvd
@@ -62,6 +76,7 @@ print(x_test.shape[0], 'test samples')
 y_train = K.utils.to_categorical(y_train, num_classes)
 y_test = K.utils.to_categorical(y_test, num_classes)
 
+# Define the model
 model = K.models.Sequential()
 model.add(K.layers.Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
